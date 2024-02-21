be94fdba493c6d69a55ebb60f170dbe04d7fdb2b
Holly Smith <hollyschuldt@gmail.com>
2024-02-20 21:28:51 -0600

    finishing this form and then getting off main oops

---
+++ api/memberData.js
@@ -0,0 +1,113 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+const getMembers = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/member.json?orderBy="uid"&equalTo="${uid}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+// CREATE Member
+const createMember = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/member.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// GET SINGLE Member
+const getSingleMember = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/member/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// DELETE Member
+const deleteSingleMember = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/member/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+//  UPDATE Member
+const updateMember = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/member/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// GET A SINGLE Member'S CHORES
+const getMemberChores = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/chore.json?orderBy="member_id"&equalTo="${firebaseKey}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+const favoriteMembers = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/member.json?orderBy="uid"&equalTo="${uid}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const favorites = Object.values(data).filter((item) => item.favorite);
+      resolve(favorites);
+    })
+    .catch(reject);
+});
+
+export {
+  getMembers,
+  createMember,
+  getSingleMember,
+  deleteSingleMember,
+  updateMember,
+  favoriteMembers,
+  getMemberChores,
+};
---
+++ components/RegisterForm.js
@@ -0,0 +1,80 @@
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useRouter } from 'next/router';
+import { FloatingLabel } from 'react-bootstrap';
+import { createMember, updateMember } from '../api/memberData';
+import { useAuth } from '../utils/context/authContext';
+
+const initialState = {
+  name: '',
+  image: '',
+  firebaseKey: '',
+};
+
+function RegisterForm({ obj }) {
+  const [formInput, setFormInput] = useState(initialState);
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (obj.firebaseKey) setFormInput(obj);
+  }, [obj]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (obj.firebaseKey) {
+      createMember(formInput)
+        .then(() => router.push('/'));
+    } else {
+      const payload = { ...formInput, uid: user.uid };
+      createMember(payload).then(({ name }) => {
+        const patchPayload = { firebaseKey: name };
+        updateMember(patchPayload).then(() => {
+          router.push('/');
+        });
+      });
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <h2 className="text-white mt-5">{obj.firebaseKey ? 'Edit your admin info..' : 'What is your name?'} </h2>
+      {/* FIRST NAME INPUT  */}
+      <FloatingLabel controlId="floatingInput1" label="First Name" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter Name"
+          name="name"
+          value={formInput.name}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+      {/* SUBMIT BUTTON  */}
+      <Button type="submit">{obj.firebaseKey ? 'UPDATE' : 'ADD'} </Button>
+    </Form>
+  );
+}
+
+RegisterForm.propTypes = {
+  obj: PropTypes.shape({
+    name: PropTypes.string,
+    image: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }),
+};
+
+RegisterForm.defaultProps = {
+  obj: initialState,
+};
+export default RegisterForm;
